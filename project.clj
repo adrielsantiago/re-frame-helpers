a7e34f0840b1620558d3f929548b69b55e2875f4
adriel <adriel.j@icloud.com>
2020-12-01 21:56:23 -0500

    0.0.1 ignore gpg signing for now

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
 (defproject hzi/re-frame-helpers "0.0.1"
   :description "Helpers to make life with re-frame easier"
   :url ""
+
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
+
+  :deploy-repositories [["clojars" {:sign-releases false}]]
+
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.773"]
                  [re-frame "1.1.2"]
